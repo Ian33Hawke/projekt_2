594f476518e281c6773a022ddcddb4a70de594d1
Ian33Hawke <michaela.a.hruskova@gmail.com>
2026-08-17 14:55:55 +0200

    přidání textu

---
+++ main.py
@@ -0,0 +1,11 @@
+#Michaela Hrušková
+
+WELCOME = [
+    '''Hi there!
+-----------------------------------------------
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+-----------------------------------------------
+Enter a number:
+-----------------------------------------------'''
+]
